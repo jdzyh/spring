be7db0179671f85676b0ca16e1b4dd1035561f7c
jdzyh <dtzyhy@163.com>
2016-10-26 16:53:28 +0800

    2016.10.26 Change java project to web project.

--- Java/SpringMVC/src/spittr/test/HomeControllerTest.java
+++ Java/SpringMVC/src/spittr/test/HomeControllerTest.java
@@ -1,10 +1,5 @@
 package spittr.test;
 
-import static org.junit.Assert.*;
-//import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-//import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-//import static org.springframework.test.web.servlet.setup.MockMvcBuilders.*;
-
 import org.junit.Test;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
